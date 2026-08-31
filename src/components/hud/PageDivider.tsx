7452a7232d5288ce7bad7dd0ab94d896df53e6cf
Laura López <soyynanii@gmail.com>
2026-08-31 11:08:09 -0500

    chore: remove old PageDivider path

--- src/components/hud/PageDivider.tsx
+++
@@ -1,9 +0,0 @@
-export default function PageDivider() {
-  return (
-    <div className="page-divider" aria-hidden="true">
-      <span className="page-divider-star">✦</span>
-      <div className="page-divider-line" />
-      <span className="page-divider-star">✦</span>
-    </div>
-  )
-}
